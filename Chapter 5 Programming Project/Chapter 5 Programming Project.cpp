a94c4d12165700588e6c7f0008a704db7f462df7
Lucian-77 <lucianshambhu@gmail.com>
2025-04-08 15:56:33 -0400

    Finished

--- Chapter 5 Programming Project/Chapter 5 Programming Project.cpp
+++ Chapter 5 Programming Project/Chapter 5 Programming Project.cpp
@@ -24,28 +24,28 @@ int main()
             std::cin >> sOrganisms;
         }
     }
-    std::cout << "\nEnter a daily population increase percentage:" << std::right << '%';
+    std::cout << std::setw(51) << std::left << "\nEnter a daily population increase percentage:" << std::right << '%';
     float popInc;
     std::cin >> popInc;
     while (popInc < 0) {
-        std::cout << "Please enter a positive increase percentage: %";
+        std::cout << std::setw(50) << std::left << "Please enter a positive increase percentage:" << std::right << '%';
         std::cin.clear();
         std::cin.ignore(256, '\n');
         std::cin >> popInc;
     }
     float popIncPer = popInc / 100;
-    std::cout << "\nHow many days would you like to simulate? ";
+    std::cout << std::setw(52) << std::left << "\nHow many days would you like to simulate?";
     float days;
     std::cin >> days;
     while (days < 1 || (days - floor(days)) != 0) {
         if (days < 1) {
-            std::cout << "Please enter a number of days greater than 1: ";
+            std::cout << std::setw(51) << std::left << "Please enter a number of days greater than 1: ";
             std::cin.clear();
             std::cin.ignore(256, '\n');
             std::cin >> days;
         }
         if ((days - floor(days)) != 0) {
-            std::cout << "Please enter a whole number of days: ";
+            std::cout << std::setw(51) << std::left << "Please enter a whole number of days: ";
             std::cin.clear();
             std::cin.ignore(256, '\n');
             std::cin >> days;
@@ -54,9 +54,9 @@ int main()
     float pop;
     for (int i = 1; i <= days; i++) {
         pop = sOrganisms * exp(popIncPer * i);
-        std::cout << std::fixed << std::setprecision(2) << "\nThe population after " << i << " days is: " << pop;
+        std::cout << std::fixed << std::setprecision(2) << "\nThe population after " << i << std::setw(17) << std::left << " days is: " << pop;
     }
-    std::cout << "\n\nThe total population after " << std::fixed << std::setprecision(0) << days << " days is: " << std::fixed << std::setprecision(2) << sOrganisms * exp(popIncPer * days) << '\n';
+    std::cout << "\n\nThe total population after " << std::fixed << std::setprecision(0) << days << " days is:  " << std::fixed << std::setprecision(2) << sOrganisms * exp(popIncPer * days) << '\n';
 
     return 0;
 }
