8775d8f7f26d5004c886957ea4178f6ea8a8296b
Lucian-77 <lucianshambhu@gmail.com>
2025-04-10 19:46:22 -0400

    Started

--- Chapter 5 Programming Project/Chapter 5 Programming Project.cpp
+++ Chapter 5 Programming Project/Chapter 5 Programming Project.cpp
@@ -10,7 +10,23 @@
 
 int main()
 {
-    //start here
+    std::cout << "What is the name of the town you wish to view? ";
+    std::string tName;
+    std::cin >> tName;
+    std::cout << "\nWhat is the name of its data file? (include name & extension): ";
+    std::string fName;
+    std::cin >> fName;
+    std::ifstream file(fName);
+    if (!file.is_open()) {
+        std::cout << "Error opening file; ensure the file named 'People.txt' is installed on youre device\n";
+    }
+    std::vector<std::string> popV;
+    std::string pop;
+    while (std::getline(file, pop)) {
+        popV.push_back(pop);
+    }
+    file.close();
+    std::cout << popV[1];
 
     return 0;
 }
